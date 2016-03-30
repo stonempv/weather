50e40c424bc8d2d8de48242c51d585cd5dd0c425
Mike Stone <michael.stone@wipro.com>
2016-03-30 10:13:28 +0100

    Initial Commit

---
+++ app/components/Home.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+var MainContainer = require('../components/MainContainer');
+
+function Home(props){
+  return (
+    <MainContainer>
+      <h1>App Title</h1>
+      <p className='lead'>Some fancy text</p>
+      <Link to='/'>
+        <button type='button' className='btn btn-large btn-success'>Get Started</button>
+      </Link>
+    </MainContainer>
+  )
+}
+
+module.exports = Home;
---
+++ app/components/Main.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
+
+function Main(props) {
+  return (
+    <div className='main-container'>
+      <ReactCSSTransitionGroup
+        transitionName="appear"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}>
+          {React.cloneElement(props.children, {key: props.location.pathname})}
+      </ReactCSSTransitionGroup>
+    </div>
+  )
+}
+
+module.exports = Main;
